4afda9abd6f57e81e175fa0f694cd0e66c5d21f9
vargajr <jeno_varga@yahoo.com>
2020-12-09 00:53:12 +0100

    more CSS done

--- js/todo.js
+++ js/todo.js
@@ -5,8 +5,36 @@ const change = () => {
     document.querySelector('.empty-list').classList.toggle('invisible');
 };
 
-document.querySelector('.input-section button').addEventListener('click', change);
+document.querySelector('.date-section').addEventListener('click', change);
 
+
+const adder = (note="ez egy todo", selected, whereTo) => {
+    const id = new Date().getTime().toString()
+    const todoContent = "ez egy todo";
+    const todoTemplateLiteral = `
+    <div class="todoContainer todo--appear">
+        <label class="todoLabel" for="${id}">
+            <input ${selected ? 'checked' : ''} type="checkbox" id="${id}">
+            <span class="checkmark"></span>
+            ${todoContent}
+        </label>
+        <button class="todoDelButton delButtonIsHiding">
+            <i class="far fa-trash-alt"></i>
+        </button>
+    </div>` 
+
+    document.querySelector(`.${whereTo}`).insertAdjacentHTML('afterbegin', todoTemplateLiteral);
+}
+
+const insertTodoDiv = () => {
+    adder('todo1', false, 'pendingItemContainer');
+};
+
+document.querySelector('.input-section button').addEventListener('click', insertTodoDiv);
+
+
+
+/* 
 console.log(document.querySelector('.pendingItemContainer div:first-child'));
 
 
@@ -17,3 +45,4 @@ const revealButton = (event) => {
 }
 
 document.querySelector('.pendingItemContainer div:first-child').addEventListener('click', revealButton);
+ */
